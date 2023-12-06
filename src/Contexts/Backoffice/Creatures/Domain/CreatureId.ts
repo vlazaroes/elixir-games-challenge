412184df5aaca0aad95bdc390e3c8d0723b61bae
Víctor Lázaro <vlazaro@outlook.es>
2023-12-06 05:15:04 +0100

    refactor(domain): change creature id type

--- src/Contexts/Backoffice/Creatures/Domain/CreatureId.ts
+++ src/Contexts/Backoffice/Creatures/Domain/CreatureId.ts
@@ -1,3 +1,3 @@
-import { Ulid } from '../../../../Contexts/Shared/Domain/Ulid';
+import { ObjectId } from '../../../../Contexts/Shared/Domain/ObjectId';
 
-export class CreatureId extends Ulid {}
+export class CreatureId extends ObjectId {}
