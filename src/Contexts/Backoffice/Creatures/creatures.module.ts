9bcadd2191dc492428a6ae7d918236c3e69b4e1a
Víctor Lázaro <vlazaro@outlook.es>
2023-12-06 05:39:23 +0100

    feat(creatures): add creature remover provider

--- src/Contexts/Backoffice/Creatures/creatures.module.ts
+++ src/Contexts/Backoffice/Creatures/creatures.module.ts
@@ -2,14 +2,19 @@ import {
     CreatureModel,
     CreatureSchema,
 } from './Infrastructure/Persistence/CreatureSchema';
-import { CreatureCreator } from '../../../Contexts/Backoffice/Creatures/Application/Create/CreatureCreator';
-import { CreatureCreatorCommandHandler } from '../../../Contexts/Backoffice/Creatures/Application/Create/CreatureCreatorCommandHandler';
+import { CreatureCreator } from './Application/Create/CreatureCreator';
+import { CreatureCreatorCommandHandler } from './Application/Create/CreatureCreatorCommandHandler';
+import { CreatureRemover } from './Application/Remove/CreatureRemover';
+import { CreatureRemoverCommandHandler } from './Application/Remove/CreatureRemoverCommandHandler';
 import { CreatureRepository } from './Domain/CreatureRepository';
 import { Module } from '@nestjs/common';
 import { MongoCreatureRepository } from './Infrastructure/Persistence/MongoCreatureRepository';
 import { MongooseModule } from '@nestjs/mongoose';
 
-export const CommandHandlers = [CreatureCreatorCommandHandler];
+export const CommandHandlers = [
+    CreatureCreatorCommandHandler,
+    CreatureRemoverCommandHandler,
+];
 
 @Module({
     imports: [
@@ -19,6 +24,7 @@ export const CommandHandlers = [CreatureCreatorCommandHandler];
     ],
     providers: [
         CreatureCreator,
+        CreatureRemover,
         { provide: CreatureRepository, useClass: MongoCreatureRepository },
         ...CommandHandlers,
     ],
