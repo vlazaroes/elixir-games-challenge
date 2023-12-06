7fc70beb1e73dcff84dbbb332a5f22c4d970d703
Víctor Lázaro <vlazaro@outlook.es>
2023-12-06 05:15:31 +0100

    refactor(domain): create object id class

--- src/Contexts/Shared/Domain/ObjectId.ts
+++ src/Contexts/Shared/Domain/ObjectId.ts
@@ -1,18 +1,18 @@
-import { isValid, ulid } from 'ulidx';
+import { ObjectId as MongoId } from 'bson';
 import { ValueObject } from './ValueObject';
 
-export class Ulid extends ValueObject<string> {
+export class ObjectId extends ValueObject<string> {
     constructor(value: string) {
         super(value);
         this.ensureValueIsValid(value);
     }
 
-    static generate(): Ulid {
-        return new Ulid(ulid());
+    static generate(): ObjectId {
+        return new ObjectId(new MongoId().toString());
     }
 
     private ensureValueIsValid(value: string): void {
-        if (!isValid(value)) {
+        if (MongoId.isValid(value)) {
             throw new Error(
                 `${this.constructor.name} does not allow the value '${value}'`,
             );
