4c5a8770f868c36608bc5c8145d9c36a7e1c3a4a
Víctor Lázaro <vlazaro@outlook.es>
2023-12-07 04:05:32 +0100

    feat(creatures): update creatures module

--- src/Contexts/Backoffice/Creatures/creatures.module.ts
+++ src/Contexts/Backoffice/Creatures/creatures.module.ts
@@ -4,17 +4,21 @@ import {
 } from './Infrastructure/Persistence/CreatureSchema';
 import { CreatureCreator } from './Application/Create/CreatureCreator';
 import { CreatureCreatorCommandHandler } from './Application/Create/CreatureCreatorCommandHandler';
+import { CreatureFinder } from './Application/SearchAll/CreatureFinder';
 import { CreatureRemover } from './Application/Remove/CreatureRemover';
 import { CreatureRemoverCommandHandler } from './Application/Remove/CreatureRemoverCommandHandler';
 import { CreatureRepository } from './Domain/CreatureRepository';
 import { Module } from '@nestjs/common';
 import { MongoCreatureRepository } from './Infrastructure/Persistence/MongoCreatureRepository';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SearchAllCreatureQueryHandler } from './Application/SearchAll/SearchAllCreatureQueryHandler';
 
 export const CommandHandlers = [
     CreatureCreatorCommandHandler,
     CreatureRemoverCommandHandler,
 ];
+
+export const QueryHandlers = [SearchAllCreatureQueryHandler];
 
 @Module({
     imports: [
@@ -23,9 +27,11 @@ export const CommandHandlers = [
         ]),
     ],
     providers: [
+        CreatureFinder,
         CreatureCreator,
         CreatureRemover,
         { provide: CreatureRepository, useClass: MongoCreatureRepository },
+        ...QueryHandlers,
         ...CommandHandlers,
     ],
 })
