1771b1b294fe7f863a129a3501ab261854fad226
Víctor Lázaro <vlazaro@outlook.es>
2023-12-07 04:38:25 +0100

    feat(core): add helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, { cors: true });
+    app.use(helmet());
+    await app.listen(3000);
 }
 bootstrap();
