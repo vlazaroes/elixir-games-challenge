bdd5cad73a8c7fa499734e3d6dbfb82c9ef96cf3
Víctor Lázaro <vlazaro@outlook.es>
2023-12-07 04:38:53 +0100

    feat(core): add mongoose and cache connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { AppCreaturesModule } from './Apps/Backoffice/Creatures/creatures.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ContextCreaturesModule } from './Contexts/Backoffice/Creatures/creatures.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot('mongodb://root:example@localhost:27017/elixir'),
+        CacheModule.register({
+            isGlobal: true,
+            store: redisStore,
+            url: 'redis://localhost:6379',
+        }),
+        AppCreaturesModule,
+        ContextCreaturesModule,
+    ],
 })
 export class AppModule {}
